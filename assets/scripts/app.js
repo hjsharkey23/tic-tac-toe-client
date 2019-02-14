f392b3db6c32cfbc46638f02814d2abca2aa1505
hjsharkey23 <hjsharkey23@gmail.com>
2019-02-14 16:35:10 -0500

    Create 'X' in div on on click
    -Added api.js, ui.js, events.js files in /tic-tac-toe
    -Update HTML game board to divs instead of buttons
    -Created event handlers to update divs in browser
    -updates app.js for event handlers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,19 @@
 'use strict'
 
 // use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+const events = require('./tic-tac-toe/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#box1').on('click', events.onClick1)
+  $('#box2').on('click', events.onClick2)
+  $('#box3').on('click', events.onClick3)
+  $('#box4').on('click', events.onClick4)
+  $('#box5').on('click', events.onClick5)
+  $('#box6').on('click', events.onClick6)
+  $('#box7').on('click', events.onClick7)
+  $('#box8').on('click', events.onClick8)
+  $('#box9').on('click', events.onClick9)
 })
---
+++ assets/scripts/tic-tac-toe/events.js
@@ -0,0 +1,40 @@
+
+const onClick1 = () => {
+  $('#box1').text('X')
+}
+const onClick2 = () => {
+  $('#box2').text('X')
+}
+const onClick3 = () => {
+  $('#box3').text('X')
+}
+const onClick4 = () => {
+  $('#box4').text('X')
+}
+const onClick5 = () => {
+  $('#box5').text('X')
+}
+const onClick6 = () => {
+  $('#box6').text('X')
+}
+const onClick7 = () => {
+  $('#box7').text('X')
+}
+const onClick8 = () => {
+  $('#box8').text('X')
+}
+const onClick9 = () => {
+  $('#box9').text('X')
+}
+
+module.exports = {
+  onClick1,
+  onClick2,
+  onClick3,
+  onClick4,
+  onClick5,
+  onClick6,
+  onClick7,
+  onClick8,
+  onClick9
+}
