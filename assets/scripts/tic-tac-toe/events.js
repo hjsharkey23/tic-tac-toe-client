85d3f937056cb68242152167e5f8d06b0515eb11
hjsharkey23 <hjsharkey23@gmail.com>
2019-02-17 20:34:10 -0500

    Add error messages for Invalid moves
    - Add error message functions in ui.js
    - Add function to determine if game is over
    - Add function to determine if clicks have been made on a space
    - Change onClick functions to allow/not allow clicks if certain
     conditions have been met, and then run appropriate function.

--- assets/scripts/tic-tac-toe/events.js
+++ assets/scripts/tic-tac-toe/events.js
@@ -12,7 +12,7 @@ const whichTurn = () => {
     return $('#user-message').text('O: it is your turn')
   }
 }
-// displays turn before anything happens: default is 'X '
+// displays turn before anything happens: default is 'X'
 whichTurn()
 
 // switches current player between x and o
@@ -23,6 +23,24 @@ const takeTurn = () => {
     currentPlayer = 'X'
   }
   return currentPlayer
+}
+
+const dubClickError = () => {
+  $('#user-message').text('Error, space is taken!')
+  setTimeout(() => {
+    $('#user-message').text('')
+    $('#user-message').text(currentPlayer + ': it is your turn')
+  }, 1500)
+}
+
+const gameOverError = () => {
+  ui.gameIsOver()
+}
+
+const draw = () => {
+  if (gameBoard.includes('') === false) {
+    console.log('draw!')
+  }
 }
 
 // checks who the winner is, keeps track of whichTurn and runs displayWinnerO,
@@ -40,35 +58,71 @@ const checkWinner = () => {
   } else if ((gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X')) {
     return ui.displayWinnerX()
   } else if ((gameBoard[6] === 'O' && gameBoard[7] === 'O' && gameBoard[8] === 'O')) {
-    return ui.displayWinnerO()
+    ui.displayWinnerO()
   } else if ((gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X')) {
-    return ui.displayWinnerX()
+    ui.displayWinnerX()
   } else if ((gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O')) {
-    return ui.displayWinnerO()
+    ui.displayWinnerO()
   } else if ((gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X')) {
-    return ui.displayWinnerX()
+    ui.displayWinnerX()
   } else if ((gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O')) {
-    return ui.displayWinnerO()
+    ui.displayWinnerO()
   } else if ((gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X')) {
-    return ui.displayWinnerX()
+    ui.displayWinnerX()
   } else if ((gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O')) {
-    return ui.displayWinnerO()
+    ui.displayWinnerO()
   } else if ((gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X')) {
-    return ui.displayWinnerX()
+    ui.displayWinnerX()
   } else if ((gameBoard[2] === 'O' && gameBoard[5] === 'O' && gameBoard[8] === 'O')) {
-    return ui.displayWinnerO()
+    ui.displayWinnerO()
   } else if ((gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X')) {
-    return ui.displayWinnerX()
+    ui.displayWinnerX()
   } else if ((gameBoard[0] === 'O' && gameBoard[4] === 'O' && gameBoard[8] === 'O')) {
-    return ui.displayWinnerO()
-    // return winner then....
+    ui.displayWinnerO()
   } else {
     console.log('still playing')
   }
 }
 
+const gameOver = () => {
+  if ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X')) {
+    console.log('hello')
+  } else if ((gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O')) {
+    return true
+  } else if ((gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X')) {
+    return true
+  } else if ((gameBoard[3] === 'O' && gameBoard[4] === 'O' && gameBoard[5] === 'O')) {
+    return true
+  } else if ((gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X')) {
+    return true
+  } else if ((gameBoard[6] === 'O' && gameBoard[7] === 'O' && gameBoard[8] === 'O')) {
+    return true
+  } else if ((gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X')) {
+    return true
+  } else if ((gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O')) {
+    return true
+  } else if ((gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X')) {
+    return true
+  } else if ((gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O')) {
+    return true
+  } else if ((gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X')) {
+    return true
+  } else if ((gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O')) {
+    return true
+  } else if ((gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X')) {
+    return true
+  } else if ((gameBoard[2] === 'O' && gameBoard[5] === 'O' && gameBoard[8] === 'O')) {
+    return true
+  } else if ((gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X')) {
+    return true
+  } else if ((gameBoard[0] === 'O' && gameBoard[4] === 'O' && gameBoard[8] === 'O')) {
+    return true
+  } else {
+    return false
+  }
+}
 // created board array to keep track of clicks
-let board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+const board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 // click variables keep track of first clicks
 // if statement checks if click has occured on space, then
@@ -76,112 +130,166 @@ let board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 // to 1 on click, runs dubClickError function to display error message.
 // if no clicks have occured, and value is 0, it runs takeTurn and checkWinner
 const onClick0 = event => {
-  if (board[0] === 0) {
+  if (gameOver() === true &&
+    board[0] === 0) {
+    gameOverError()
+  } else if (board[0] === 0) {
     $(event.target).text(currentPlayer)
     board[0] += 1
     gameBoard[0] = currentPlayer
     takeTurn()
     checkWinner()
     console.log(gameBoard)
+  } else if (board[0] === 1 &&
+    gameOver() === true) {
+    gameOverError()
   } else if (board[0] === 1) {
-    ui.dubClickError()
+    dubClickError()
   }
 }
 const onClick1 = event => {
-  if (board[1] === 0) {
+  if (gameOver() === true &&
+    board[1] === 0) {
+    gameOverError()
+  } else if (board[1] === 0) {
     $(event.target).text(currentPlayer)
     board[1] += 1
     gameBoard[1] = currentPlayer
     takeTurn()
     checkWinner()
     console.log(gameBoard)
+  } else if (board[1] === 1 &&
+    gameOver() === true) {
+    gameOverError()
   } else if (board[1] === 1) {
-    ui.dubClickError()
+    dubClickError()
   }
 }
 const onClick2 = event => {
-  if (board[2] === 0) {
+  if (gameOver() === true &&
+    board[2] === 0) {
+    gameOverError()
+  } else if (board[2] === 0) {
     $(event.target).text(currentPlayer)
     board[2] += 1
     gameBoard[2] = currentPlayer
     takeTurn()
     checkWinner()
     console.log(gameBoard)
+  } else if (board[2] === 1 &&
+    gameOver() === true) {
+    gameOverError()
   } else if (board[2] === 1) {
-    ui.dubClickError()
+    dubClickError()
   }
 }
 
 const onClick3 = event => {
-  if (board[3] === 0) {
+  if (gameOver() === true &&
+    board[3] === 0) {
+    gameOverError()
+  } else if (board[3] === 0) {
     $(event.target).text(currentPlayer)
     board[3] += 1
     gameBoard[3] = currentPlayer
     takeTurn()
     checkWinner()
     console.log(gameBoard)
+  } else if (board[3] === 1 &&
+    gameOver() === true) {
+    gameOverError()
   } else if (board[3] === 1) {
-    ui.dubClickError()
+    dubClickError()
   }
 }
 const onClick4 = event => {
-  if (board[4] === 0) {
+  if (gameOver() === true &&
+    board[4] === 0) {
+    gameOverError()
+  } else if (board[4] === 0) {
     $(event.target).text(currentPlayer)
     board[4] += 1
     gameBoard[4] = currentPlayer
     takeTurn()
     checkWinner()
     console.log(gameBoard)
+  } else if (board[4] === 1 &&
+    gameOver() === true) {
+    gameOverError()
   } else if (board[4] === 1) {
-    ui.dubClickError()
+    dubClickError()
   }
 }
 const onClick5 = event => {
-  if (board[5] === 0) {
+  if (gameOver() === true &&
+    board[5] === 0) {
+    gameOverError()
+  } else if (board[5] === 0) {
     $(event.target).text(currentPlayer)
     board[5] += 1
     gameBoard[5] = currentPlayer
     takeTurn()
     checkWinner()
     console.log(gameBoard)
+  } else if (board[5] === 1 &&
+    gameOver() === true) {
+    gameOverError()
   } else if (board[5] === 1) {
-    ui.dubClickError()
+    dubClickError()
   }
 }
 const onClick6 = event => {
-  if (board[6] === 0) {
+  if (gameOver() === true &&
+    board[6] === 0) {
+    gameOverError()
+  } else if (board[6] === 0) {
     $(event.target).text(currentPlayer)
     board[6] += 1
     gameBoard[6] = currentPlayer
     takeTurn()
     checkWinner()
     console.log(gameBoard)
+  } else if (board[6] === 1 &&
+    gameOver() === true) {
+    gameOverError()
   } else if (board[6] === 1) {
-    ui.dubClickError()
+    dubClickError()
   }
 }
 const onClick7 = event => {
-  if (board[7] === 0) {
+  if (gameOver() === true &&
+    board[7] === 0) {
+    gameOverError()
+  } else if (board[7] === 0) {
     $(event.target).text(currentPlayer)
     board[7] += 1
     gameBoard[7] = currentPlayer
     takeTurn()
     checkWinner()
     console.log(gameBoard)
+  } else if (board[7] === 1 &&
+    gameOver() === true) {
+    gameOverError()
   } else if (board[7] === 1) {
-    ui.dubClickError()
+    dubClickError()
   }
 }
 const onClick8 = event => {
-  if (board[8] === 0) {
+  if (gameOver() === true &&
+    board[8] === 0) {
+    gameOverError()
+  } else if (board[8] === 0) {
     $(event.target).text(currentPlayer)
     board[8] += 1
     gameBoard[8] = currentPlayer
     takeTurn()
     checkWinner()
     console.log(gameBoard)
+  } else if (board[8] === 1 &&
+    gameOver() === true) {
+    gameOverError()
   } else if (board[8] === 1) {
-    ui.dubClickError()
+    dubClickError()
   }
 }
 
--- assets/scripts/tic-tac-toe/ui.js
+++ assets/scripts/tic-tac-toe/ui.js
@@ -1,11 +1,11 @@
 // create function to display error messages, passed to events.js
-const dubClickError = () => {
-  $('#user-message').text('Error, space is taken!')
-  setTimeout(() => {
-    $('#user-message').text('')
-    $('#user-message').removeClass('Error, space is taken!')
-  }, 1500)
-}
+// const dubClickError = () => {
+//   $('#user-message').text('Error, space is taken!')
+//   setTimeout(() => {
+//     $('#user-message').text('')
+//     $('#user-message').removeClass('Error, space is taken!')
+//   }, 1500)
+// }
 
 const displayWinnerX = () => {
   $('#user-message').text('X wins!')
@@ -15,8 +15,12 @@ const displayWinnerO = () => {
   $('#user-message').text('O wins!')
 }
 
+const gameIsOver = () => {
+  $('#user-message').text('Game is over, please reset the board!')
+}
+
 module.exports = {
-  dubClickError,
   displayWinnerX,
-  displayWinnerO
+  displayWinnerO,
+  gameIsOver
 }
